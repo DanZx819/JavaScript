e4abff53cf26c34c9a7f3d90073dcf3730254184
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-30 13:05:59 -0300

    Create index.js

---
+++ aula 11/js/index.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Digite o n1: ');
+let num2 = prompt('Digite o n2: ');
+
+num1 = Number(num1);
+num2 = Number(num2);
+let resultado = num1 + num2;
+
+alert(`Resultado ${resultado}`);
