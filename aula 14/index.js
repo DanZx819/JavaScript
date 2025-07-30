e6abd6ecfd024c984792e8b0997768e38ee1c6a9
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-30 14:31:24 -0300

    Create index.js

---
+++ aula 14/index.js
@@ -0,0 +1,20 @@
+let num1 = 1;
+let num2 = 2.5;
+
+//console.log(num1 + num2);
+//console.log(num1.toString() + num2);
+
+num1 = 1500;
+
+console.log(num1.toString(2));
+
+num1 =  10.59348343;
+console.log(num1.toFixed(2));
+
+num1 = 10;
+
+console.log(Number.isInteger(num1));
+
+let temp = num1 * 'Ola';
+
+console.log(Number.isNaN(temp));
