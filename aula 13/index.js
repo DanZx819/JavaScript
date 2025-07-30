99b67a7a253110f8c10004765da0f58f5cb8e366
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-30 14:31:22 -0300

    Create index.js

---
+++ aula 13/index.js
@@ -0,0 +1,61 @@
+//let umaString = "Um \"texto\"";
+//let outraString = "Um \\texto";
+
+//console.log(umaString);
+//console.log(outraString);
+
+//               01234567      
+//let umaString = "Um texto";
+
+//console.log(umaString[8]);
+
+//let umaString = "Um texto";
+
+//console.log(umaString.charAt());
+
+//console.log(`${umaString} em um lindo dia!`);
+
+//               01234567      
+//let umaString = "Um texto";
+//console.log(umaString.indexOf('o', 3));
+
+//               01234567      
+//let umaString = "Um texto";
+//console.log(umaString.lastIndexOf('m', 3));
+
+//               01234567      
+//let umaString = "Um texto";
+//console.log(umaString.match(/[a-z]/));
+
+//               01234567      
+//let umaString = "Um texto";
+//console.log(umaString.search(/x/));
+
+//               01234567      
+//let umaString = "Um texto";
+//console.log(umaString.replace('Um', 'Outro'));
+
+//               01234567      
+//let umaString = "O rato roeu a roupa do rei de roma";
+//console.log(umaString.replace(/r/g, '#'));
+
+//                 01234567      
+//let umaString = "O rato roeu a roupa do rei de roma";
+//console.log(umaString.length);
+
+//               01234567      
+//let umaString = "O rato roeu a roupa do rei de roma";
+//console.log(umaString.slice(2, 6));
+
+//               01234567      
+//let umaString = "O rato roeu a roupa do rei de roma.";
+//console.log(umaString.substring(umaString.length - 5));
+
+//               01234567      
+//let umaString = "O rato roeu a roupa do rei de roma";
+//console.log(umaString.split(' ', 3));
+
+//               01234567      
+//let umaString = "O rato roeu a roupa do rei de roma";
+//console.log(umaString.toUpperCase());
+//console.log(umaString.toLocaleLowerCase());
