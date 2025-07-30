bbdaaa2c77b3ae9c272fd1dcd0a30c7a63470583
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-30 14:31:25 -0300

    Create index.js

---
+++ aula 15/index.js
@@ -0,0 +1,16 @@
+let num1 = 9.54568;
+let num2 = Math.floor(num1);
+let num3 = Math.ceil(num1)
+let num4 = Math.round(num1);
+let num5 = 9;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+console.log(Math.max(1,2,3,4,5,6,7,8,0,-1,-2));
+console.log(Math.min(1,2,3,4,5,6,7,8,0,-1,-2));
+console.log(Math.round(Math.random() * (10)));
+console.log(Math.PI);
+console.log(num5 ** 0.5);
