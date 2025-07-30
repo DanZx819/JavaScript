934ac6cd9e19bb0a64a01188c92ad41bc2300d2f
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-30 20:00:52 -0300

    Estudando Arrays no JS

---
+++ aula 16/index.js
@@ -0,0 +1,37 @@
+
+//                0        1        2 
+const alunos = ['Luiz', 'Maria', 'João'];
+
+// alunos[alunos.length] = 'Luiza';
+
+// console.log(alunos.length); Pega o tamanho do array
+
+// alunos.push('Daniel'); //Adiciona no ultimo indice
+// alunos.unshift('Luiza'); //Adiciona no primeiro indice
+
+
+
+// alunos[0] = 'Eduardo'; //Adiciona ou muda onde você indicar
+// alunos[3] = 'Luiza';
+
+
+
+//console.log(alunos[0]); 
+//console.log(alunos[1]);
+//console.log(alunos[2]);
+
+// alunos.pop(); //Remove o ultimo indice
+// const removido = alunos.pop(); //Da pra armazenar em uma variavel
+
+// console.log(removido);
+
+//alunos.shift(); Remove do começo da pra armazenar em uma variavel tambem
+
+//delete alunos[1]; Deleta mas não remove o indice
+
+//console.log(alunos);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -2)); //Fatiamento de array
