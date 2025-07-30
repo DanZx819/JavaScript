52739ecfdcaa89c43f440f052c3e862d2c1a1264
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-30 13:06:16 -0300

    Create index.js

---
+++ aula 8/index.js
@@ -0,0 +1,19 @@
+const nome = "Daniel";
+
+const sobrenome = "Zanchetta";
+
+const idade = 18;
+
+const altura = 1.76;
+
+const peso = 66;
+
+let imc;
+
+imc = peso / (altura * altura);
+
+console.log(nome, sobrenome, "tem ", idade, "anos, pesa", peso, "kg tem", altura, "de altura e seu IMC é de", imc );
+
+let anoNascimento = 2025 - idade;
+
+console.log(nome, sobrenome, "nasceu em", anoNascimento);
