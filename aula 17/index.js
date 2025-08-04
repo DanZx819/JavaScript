6e19b2f22e32e959e04ec7dc6ff9fee2b921a90b
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-03 23:59:19 -0300

    Aula de função

--- aula 17/index.js
+++ aula 17/index.js
@@ -1,5 +1,30 @@
-function saudacao(nome){
-    console.log(`Bom dia ${nome}!`);
+// function saudacao(nome){
+//     //console.log(`Bom dia ${nome}!`);
+
+//     return `Bom dia ${nome}!`;
+// };
+
+// saudacao("Daniel");
+
+
+// const variavel = saudacao('Daniel');
+// console.log(variavel);
+
+// function soma(x = 1, y = 1){
+//     const resultado = x + y;
+//     return resultado;
+// };
+
+// console.log(soma(2, 5));
+// console.log(soma(4, 7));
+// console.log(soma(2, 2));
+// console.log(soma());
+
+const raiz = (n) => {
+    return n ** 0.5;
 };
 
-saudacao("Daniel");
+
+console.log(raiz(9));
+
+console.log(raiz());
