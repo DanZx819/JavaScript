236373137fec058e5bbabc311ea65a0e6b286c6f
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-04 00:21:39 -0300

    Aula de Primitivos e por referência

---
+++ aula 19/index.js
@@ -0,0 +1,9 @@
+/*
+
+Primitivos (imutáveis) - string, number, boolean, undefined, null e (bigint, symbol) - Valor
+
+Referência (mutável) - array, object, function - Passados por referência
+
+Aula teórica
+
+*/
