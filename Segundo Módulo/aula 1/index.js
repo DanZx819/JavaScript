eaea64b15546e9c94893fe26e64e08c6cc69a77f
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-05 00:38:24 -0300

    Logico de comparadores

---
+++ Segundo Módulo/aula 1/index.js
@@ -0,0 +1,23 @@
+/**
+ * Operadores de Comparação
+ * 
+ * > maior que
+ * >= maior ou igual que
+ * < menor que 
+ * <= menor ou igual que
+ * == igualdade (valor)
+ * === igualdade (valor e tipo)
+ * != diferença (valor)
+ * !== diferença (valor e tipo)
+ */
+
+const comp = 10 > 5;
+const comp2 = 10 <= 5;
+const comp3 = 10 === 5;
+const comp4 = 10 !== 5;
+
+
+console.log(comp);
+console.log(comp2);
+console.log(comp3);
+console.log(comp4);
