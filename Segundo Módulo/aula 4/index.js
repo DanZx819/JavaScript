dfe41db347edb9e0e9785bd1fb11daaafbae358e
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-05 01:04:07 -0300

    Else if elseif

---
+++ Segundo Módulo/aula 4/index.js
@@ -0,0 +1,26 @@
+/**
+ * If -> pode ser usado sozinho
+ * Else if -> Não pode ser usado sozinho más pode ser utilizado infinitamente
+ * Else -> Só pode ter 1
+ */
+
+
+// const hora = 18;
+
+// if (hora <= 11 && hora >= 6) {
+//     console.log('Bom dia !');
+// }else if(hora > 11 && hora <= 17){
+//     console.log('Boa tarde')
+// }else if(hora <= 23 && hora <= 5){
+//     console.log('Boa noite');
+// }else{
+//     console.log('Olá tenha um bom dia !')
+// }
+
+const tenhoGrana = true;
+
+if(tenhoGrana){
+    console.log('Vou sair de casa');
+}else{
+    console.log('Não vou sair de casa')
+}
