e7f24560f33e25c8582de6040f9c4534ad223a4b
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-05 00:44:44 -0300

    Operadores Lógicos

---
+++ Segundo Módulo/aula 2/index.js
@@ -0,0 +1,19 @@
+/**
+ * && -> AND -> E -> Todas precisam ser verdadeiras
+ * || -> OR -> OU -> Uma precisa ser verdadeira
+ * ! -> NOT -> NÃO -> Nega uma expressão 
+ */
+
+// console.log(true && true);
+// console.log(false && true);
+// console.log(false || true);
+// console.log(true || true);
+
+
+// const usuario = 'Luiz';
+// const senha = '123456';
+
+// const vaiLogar = usuario === 'Luiz' && senha === '123456';
+// console.log(vaiLogar);
+
+console.log(!false);
