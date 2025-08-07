05c3efca78a17761c9e05b918a0fb4353268e5e0
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-07 01:19:31 -0300

    Aula teórica

---
+++ Segundo Módulo/aula 11/index.js
@@ -0,0 +1,25 @@
+//Diferenças de Var e Let parte 2
+
+//Let tem escopo de bloco {...Bloco}
+//Var só tem escopo de função
+
+// const verdadeira = true;
+
+// let nome = 'Luiz';
+
+// var nome2 = 'Luiz';
+
+// var nome2 = 'Daniel';
+
+
+
+// if (verdadeira) {
+//     let nome = 'Otavio';
+//     console.log(nome, nome2);
+// }
+
+// if(verdadeira){
+//     console.log(nome, nome2);
+// }
+
+//Aula Full teórica
