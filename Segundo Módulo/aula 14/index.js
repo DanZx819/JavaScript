fa24a0695d466696c4e4ff2e488db0aec864a81e
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-07 23:54:43 -0300

    For classico

---
+++ Segundo Módulo/aula 14/index.js
@@ -0,0 +1,32 @@
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// i - index
+
+// for (let i = 0; i <= 5; i++){
+//     console.log(`Linha ${i}`)
+// }
+
+// for (let i = 500; i >= 400; i-= 10){
+//     console.log(`Linha ${i}`)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//        console.log(i, 'PAR') 
+//     }else{
+//         console.log(i, 'ÍMPAR')
+//     }
+    
+// }
+
+
+const frutas = ['Maçã', 'Pera', 'Uva', 'Morango'];
+
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(`Índice ${i}`, frutas[i]);
+}
