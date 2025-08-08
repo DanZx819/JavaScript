6680896a7b25e06b1655aa4840902dae71d1bb10
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-08 01:07:17 -0300

    while e do while

---
+++ Segundo Módulo/aula 20/index.js
@@ -0,0 +1,35 @@
+// const nome = 'Luiz';
+// let i = 0;
+
+// while(i < nome.length){
+//     console.log(nome[i]);
+//     i++
+// }
+
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+let rand = random(1, 50);
+let cont = 0;
+// while(rand !== 10){
+//     rand = random(1,50);
+//     console.log(`${rand}`);
+//     if(rand !== 10){
+//         cont++
+//     }else{
+//         console.log(`foi ${cont} vezes até chegar no 10`)
+//     }
+
+// }
+
+do {
+    rand = random(1, 100000000);
+    console.log(`${rand}`);
+    if (rand !== 10) {
+        cont++
+    } else {
+        console.log(`foi ${cont} vezes até chegar no 10`)
+    }
+} while (rand !== 10);
