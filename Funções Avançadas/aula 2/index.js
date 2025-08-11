e4fb6d4e5b9732ebfaf9b0e83c5d5eec8861c92f
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-10 22:57:55 -0300

    parametros pt 2

--- Funções Avançadas/aula 2/index.js
+++ Funções Avançadas/aula 2/index.js
@@ -1,7 +1,46 @@
 //Parametros - Parei em 3:49
 
-function funcao() {
-    console.log(arguments);
+// function funcao() {
+//     let total = 0;
+//     for (let argumento of arguments){
+//         total += argumento;
+//     }
+
+//     console.log(total)
+// }
+
+// funcao(1, 2);
+
+// function funcao(a,b,c,d,e,f){
+//     console.log(a,b,c,d,e,f);
+// }
+
+// funcao(1,2,3);
+
+// function funcao([nome, sobrenome, idade]){
+//     console.log(nome, sobrenome, idade);
+// }
+
+// funcao(['Daniel', 'Zanchetta', 18]);
+
+// function funcao2({nome, sobrenome, idade}){
+//     console.log(nome, sobrenome, idade);
+// }
+// // funcao(2, undefined, 20);
+
+// let obj = {nome: 'Luiz', sobrenome: 'Otávio', idade: 18};
+
+// funcao2(obj);
+
+const conta = function (operador, acumulador, ...numeros){
+    for(let numero of numeros){
+        if(operador === '+') acumulador += numero;
+        if(operador === '-') acumulador -= numero;
+        if(operador === '*') acumulador *= numero;
+        if(operador === '/') acumulador /= numero;
+    }
+
+    console.log(acumulador);
 }
 
-funcao('Valor', 1, 2);
+conta('+', 0, 20,30,40,50);
