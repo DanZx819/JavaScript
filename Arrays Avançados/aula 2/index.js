1e145b41af47145cdc43cb533d767d7e8fccf07c
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-13 01:54:08 -0300

    Função Splice

---
+++ Arrays Avançados/aula 2/index.js
@@ -0,0 +1,20 @@
+//
+
+const nomes = new Array('Eduardo', 'Maria', 'Joana', 'Cleiton', 'Julia', 'Sofia'); // Construtor
+
+// nomes.slice(indice, delete, elem1, elem2, elem3);
+// pop
+
+// const removidos = nomes.splice(-2, Number.MAX_VALUE);
+
+// const adicionados = nomes.splice(0, 2, 'Daniel', 'Colonhezi');
+
+// const pop = nomes.splice(-1, 1);
+
+//const shift = nomes.splice(0, 1);
+
+//const unshift = nomes.splice(0, 0, 'Luiz');
+
+// const push = nomes.splice(nomes.length, 1, 'Luiz', 'Otávio');
+
+console.log(nomes, push);
