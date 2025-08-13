be4881106b5e3d4a9a6c1bcf48e0a146d77a4786
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-13 01:58:57 -0300

    Concatenando Arrays

---
+++ Arrays Avançados/aula 3/index.js
@@ -0,0 +1,14 @@
+//Concatenar Arrays
+
+const a1 = [1,2,3];
+const a2 = [4,5,6];
+
+// const a3 = a1 + a2; Jeito errado
+const a3 = a1.concat(a2, [7,8,9], 'Daniel'); // Jeito certo
+
+// ...
+
+// const a3 = [...a1, 'Daniel', ...a2]
+
+
+console.log(a3, )
