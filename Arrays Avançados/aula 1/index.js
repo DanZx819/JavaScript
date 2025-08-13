63640585adbd32c32b04f3d6f8696d045c0257f3
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-13 01:37:21 -0300

    Revisão de Array

---
+++ Arrays Avançados/aula 1/index.js
@@ -0,0 +1,36 @@
+// Revisão de Arrays
+
+// const nomes = ['Eduardo', 'Maria', 'Joana'];
+
+// String, Objetos, Funções, Números
+
+// const nomes = new Array('Eduardo', 'Maria', 'Joana', 'Cleiton'); // Construtor
+// const novo = [...nomes];
+
+// nomes[2] = 'João';
+// // delete nomes[2];
+// novo.pop();
+// console.log(nomes);
+// console.log(novo)
+
+// console.log(nomes.length)
+
+// const removido = nomes.pop();
+// const removido2 = nomes.shift();
+
+// console.log(nomes, removido, removido2)
+
+// nomes.push('Daniel'); // Add no ultimo (Não meche nos indices)
+// nomes.unshift('Martins'); //Add no primeiro (Move os indices)
+
+// const novo = nomes.slice(1,-2);
+
+// const nome = 'Luiz Otávio Miranda';
+
+// const nomes = nome.split(' ');
+
+
+const nomes = ['Daniel', 'de', 'Oliveira' ]
+const nome = nomes.join(' ');
+
+console.log(nome);
