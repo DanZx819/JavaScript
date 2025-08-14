6a4e8e0dea1937f1f0083cc6491e33dc7d9669d3
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-14 00:07:16 -0300

    Aula de reduce

--- Arrays Avançados/aula 6/index.js
+++ Arrays Avançados/aula 6/index.js
@@ -17,25 +17,32 @@ const pares = numeros.reduce((acumulador, valor) =>{
     return acumulador;
 }, [])
 
-const dobro = numeros.map((valor) =>{
-    
-    let dobroA = [];
-    valor *= 2;
-    dobroA.splice(0,0,valor)
-    return dobroA;
-})
+
 
 console.log(total);
 console.log(pares);
-console.log(dobro)
 
+
+//Retorne a pessoa mais velha
 
 
 const pessoas = [
-    {nome: 'Daniel', idade: 18},
+    {nome: 'Daniel', idade: 48},
     {nome: 'Joao', idade: 8},
     {nome: 'Pedro', idade: 16},
     {nome: 'Julia', idade: 40},
     {nome: 'Sofia', idade: 37},
     {nome: 'Gonçalves', idade: 18},
 ];
+
+const velha = pessoas.reduce((acumulador, valor)=>{
+
+
+    if (acumulador.idade > valor.idade) {
+        return acumulador;
+    }
+    return valor;
+
+})//Se não tem um número pré definido o acumulador é o prieiro item do array
+
+console.log(velha);
