301e4d7fabbeba3bcad1675af242de7e246f214c
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-26 01:42:57 -0300

    Update app.js

--- NodeJS, NPM, Express/aula 2/app.js
+++ NodeJS, NPM, Express/aula 2/app.js
@@ -1,10 +1,15 @@
-const mod1 = require('./B/C/D/mod');
-const mult = mod1.mult;
-const Cachorro = mod1.Cachorro;
+// const mod1 = require('./B/C/D/mod');
+// const mult = mod1.mult;
+// const Cachorro = mod1.Cachorro;
 
 
-console.log(mult(4, 2))
+// console.log(mult(4, 2))
 
-const dog = new Cachorro('Gonçagay')
-dog.latir();
-// console.log();
+// const dog = new Cachorro('Gonçagay')
+// dog.latir();
+// // console.log();
+
+const path = require('path');
+console.log(__dirname)
+console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'));
+console.log(path.resolve(__dirname, '.', '.', 'arquivos', 'imagens'));
