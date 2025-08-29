5d7587f127d72f3028057e037b4feb36c35047b8
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-29 14:54:42 -0300

    CSRF e milddlewares

---
+++ NodeJS-NPM-Express/aula 17/src/middlewares/middleware.js
@@ -0,0 +1,20 @@
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.varLocal = 'Este é o valor da variavel local.';
+    next();
+}
+
+exports.outroMiddleware = (req, res, next) =>{
+    next();
+}
+
+exports.checkCsrfError = function(err, req, res, next){
+    if (err && err.code === 'EBADCSRFTOKEN') {
+        return res.render('error/404')
+    }
+    next(err)
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next()
+}
