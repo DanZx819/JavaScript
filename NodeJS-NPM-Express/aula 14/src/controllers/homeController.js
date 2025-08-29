5122c7666f96574889367ce711ddecc7a60944c2
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-08-29 11:46:05 -0300

    Criando dados

--- NodeJS-NPM-Express/aula 14/src/controllers/homeController.js
+++ NodeJS-NPM-Express/aula 14/src/controllers/homeController.js
@@ -4,6 +4,24 @@
 //   console.log(`Olha o que tem na req.session.nome ${req.session.nome}`)
 //   next()
 // };
+
+const HomeModel = require('../models/HomeModel');
+
+
+// HomeModel.create({
+//   titulo: 'Um titulo de testes',
+//   descricao: 'Uma descricao de testes'
+// }).then(dados => {
+//   console.log(dados);
+// }).catch((e) => console.log(e));
+
+// HomeModel.find({
+//   titulo: 'Um titulo de testes',
+//   descricao: 'Uma descricao de testes'
+// }).then(dados => {
+//   console.log(dados);
+// }).catch((e) => console.log(e));
+
 exports.paginaInicial = (req, res,  next) => {
   res.render('index');
 };
