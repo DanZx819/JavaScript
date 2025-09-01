1f9b941af6553952cfe0d5f5310825a63ce94042
Daniel de Oliveira Zanchetta <danielzanchetta11@gmail.com>
2025-09-01 14:44:41 -0300

    Logando Usuario

--- Projeto Agenda - Node-JS/src/controllers/loginController.js
+++ Projeto Agenda - Node-JS/src/controllers/loginController.js
@@ -1,7 +1,10 @@
 const Login = require("../models/LoginModel");
 
 exports.index = (req, res, next) => {
-  res.render("login");
+  if(req.session.user){
+    return res.render("logado");
+  }
+  return res.render("login");
 };
 
 exports.register = async (req, res) => {
@@ -24,3 +27,32 @@ exports.register = async (req, res) => {
     return res.render("./error/404");
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("/login");
+      });
+      return;
+    }
+    req.flash("success", "Você entrou no sistema.");
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect("/login");
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render("./error/404");
+  }
+};
+
+exports.logout = function(req, res){
+  req.flash("errors", "Você foi deslogado!");
+  req.session.destroy();
+  
+  res.redirect('/');
+}
