f79479ef71f86845cc20595975bc65469e1dad5a
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-09-03 01:12:47 -0300

    Finalizando

--- Projeto Agenda - Node-JS/src/controllers/homeController.js
+++ Projeto Agenda - Node-JS/src/controllers/homeController.js
@@ -1,20 +1,23 @@
 const HomeModel = require("../models/HomeModel");
-const Contato = require('../models/ContatoModel')
+const Contato = require("../models/ContatoModel");
 exports.paginaInicial = async (req, res, next) => {
-  try {
-    const contatos = await Contato.buscaContato(req.session.user._id);
+  if (req.session.user) {
+    try {
+      const contatos = await Contato.buscaContato(req.session.user._id);
+
+      res.render("index", {
+        contatos: contatos || [], // garante sempre um array
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(400).render("./error/404"); // página de erro genérica
+    }
+  }else{
     res.render("index", {
-    contatos: contatos
-  });
-  } catch (e) {
-    console.log(e);
-    res.render('./error/404');
+        contatos: {}, // garante sempre um array
+      });
   }
-  
-  return;
 };
-
-
 
 exports.trataPost = (req, res) => {
   res.send(req.body);
