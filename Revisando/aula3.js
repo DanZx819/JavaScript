f7108deac25122279f20e16c210ab1834fe5dd3b
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-03-21 11:27:47 -0300

    Create aula3.js

---
+++ Revisando/aula3.js
@@ -0,0 +1,41 @@
+
+
+const numeros = [5, 50, 80, 1,2,3,5,8,7,11,15,22,27];
+//Some todos os números (reduce)
+const total = numeros.reduce((acumulador, valor)=>{
+    return acumulador + valor;
+}, 0)
+
+const pares = numeros.filter((valor) =>{
+    return valor % 2 === 0;
+})
+
+const somarPares = pares.reduce((acumulador, valor) =>{
+    return acumulador + valor;
+}, 0)
+
+const dobro = numeros.map((valor) =>{
+    return valor * 2;
+})
+/* console.log(pares);
+console.log(somarPares)
+console.log(total);
+console.log(dobro); */
+
+const pessoas = [
+    {nome:"Luiz", idade:62},
+    {nome:"Daniel", idade:74},
+    {nome:"Daniela", idade:80},
+    {nome:"Bruna", idade:55},
+    {nome:"Jorge", idade:19}
+]
+
+const pessoaMaisVelha = pessoas.reduce((acumulador, obj) =>{
+    
+    if(obj.idade > acumulador.idade){
+        return obj;
+    }
+    return acumulador
+});
+
+console.log(pessoaMaisVelha)
